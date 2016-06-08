175d8a7e8f7b1b6da0ed244d720a79477f82bfd8
Danny Hurlburt <dhurlburtusa@users.noreply.github.com>
2016-06-08 15:39:15 -0600

    Remove unused code.
    The code removed was intended for the QC library (WIP).

--- etc/webpack-entry.js
+++ etc/webpack-entry.js
@@ -1,16 +1,2 @@
 
-// Save the previous value of the `QC` variable.
-var previousQc = global.QC;
-
-const QC = require('.');
-
-/**
- * Run QC.js in *noConflict* mode, returning the `QC` variable to its previous owner.  Returns a reference to the QC
- * object.
- */
-QC.noConflict = function () {
-  global.QC = previousQc;
-  return this;
-};
-
-global.QC = QC;
+global.QC = require('.');
